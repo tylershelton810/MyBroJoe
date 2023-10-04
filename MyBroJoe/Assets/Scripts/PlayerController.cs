c7f767c371bb7219e32a448935d38fe82e670896
tylershelton810 <tylershelton810@gmail.com>
2023-10-04 09:14:59 -0400

    Added the ground check

--- MyBroJoe/Assets/Scripts/PlayerController.cs
+++ MyBroJoe/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float jumpingPower = 15f;
     private Rigidbody rb;
+    private bool isGrounded = true;
+
+    [SerializeField] private float jumpingPower = 15f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +21,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    //This is a default method that runs everytime the player collider hits another collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        //The tag is set on the ground object. Any object that has the tag ground will tell us that the player is grounded
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    //This is a default method that runs everytime the player collider leaves contact with another collider
+    private void OnCollisionExit(Collision collision)
+    {
+        // If the collider that was left is a ground we are no longer grounded. We also check that our y velocity is positive. This
+        // means that we are moving upward and the jump button is hit. If the ball rolls off of a platform this would not allow the
+        // player to jump. 
+        if (collision.collider.CompareTag("Ground") && rb.velocity.y > .01)
+        {
+            isGrounded = false;
+        }
     }
 
     // Triggers Jump
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(context.started)
+        if (context.started)
         {
-            Jump();
+            if (isGrounded)
+            {
+                Jump();
+            }
         }
 
     }
